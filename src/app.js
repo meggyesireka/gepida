48db54e7ca09e28bcb8776c1ac9b5ec6c45f208e
meggyesireka <meggyesireka@gmail.com>
2023-03-08 19:11:11 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File: app.js
+* Author: Meggyesi Réka
+* Copyright: 2023, Meggyesi Réka
+* Group: SZOFT I/1/E
+* Date: 2023-03-08
+* Github: https://github.com/meggyesireka/
+* Licenc: GNU GPL
+*/
